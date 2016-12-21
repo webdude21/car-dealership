04fddab1c81fcaff39d3d259554170af750c0a5e
webdude <dimo.petrof@gmail.com>
2016-12-21 18:12:37 +0200

    did the initial project setup

---
+++ src/main/java/eu/webdude/CarDealershipApplication.java
@@ -0,0 +1,12 @@
+package eu.webdude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarDealershipApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarDealershipApplication.class, args);
+	}
+}
