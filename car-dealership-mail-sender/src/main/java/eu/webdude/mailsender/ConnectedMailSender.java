e6e18e1aa10bd3f68e2442be338831236e6f71da
webdude <dimo.petrof@gmail.com>
2017-06-03 21:17:05 +0300

    refactored a bit

--- car-dealership-mail-sender/src/main/java/eu/webdude/mailsender/ConnectedMailSender.java
+++ car-dealership-mail-sender/src/main/java/eu/webdude/mailsender/ConnectedMailSender.java
@@ -1,5 +1,7 @@
 package eu.webdude.mailsender;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.annotation.JmsListener;
@@ -14,6 +16,8 @@ import javax.mail.internet.MimeMessage;
 public class ConnectedMailSender {
 	private JavaMailSender javaMailSender;
 
+	private static final Logger log = LoggerFactory.getLogger(ConnectedMailSender.class);
+
 	@Value("${email.destination}")
 	private String destination;
 
@@ -27,22 +31,23 @@ public class ConnectedMailSender {
 
 	@JmsListener(destination = "message-queue")
 	public void messageListener(org.springframework.messaging.Message<String> message) {
-		sendEmail(message.getPayload());
-		System.out.printf("Message was sent with the following content: %n %s%n", message.getPayload());
-	}
-
-	private void sendEmail(String messageBody) {
-		MimeMessage mail = javaMailSender.createMimeMessage();
 		try {
-			MimeMessageHelper helper = new MimeMessageHelper(mail, true);
-			helper.setTo(destination);
-			helper.setReplyTo(sender);
-			helper.setFrom(sender);
-			helper.setSubject("A car has been deleted");
-			helper.setText(messageBody);
+			sendEmail(message.getPayload());
+			log.debug(String.format("Message was sent with the following content: %n %s%n", message.getPayload()));
 		} catch (MessagingException e) {
+			log.warn("Sending email failed");
 			e.printStackTrace();
 		}
+	}
+
+	private void sendEmail(String messageBody) throws MessagingException {
+		MimeMessage mail = javaMailSender.createMimeMessage();
+		MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+		helper.setTo(destination);
+		helper.setReplyTo(sender);
+		helper.setFrom(sender);
+		helper.setSubject("A car has been deleted");
+		helper.setText(messageBody);
 		javaMailSender.send(mail);
 	}
 }
