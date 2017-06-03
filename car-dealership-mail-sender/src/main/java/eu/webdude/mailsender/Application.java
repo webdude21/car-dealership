79ff2e82397370ac8fba4df69dd428fab0b56f08
webdude <dimo.petrof@gmail.com>
2017-06-03 21:00:33 +0300

    sending mail works

--- car-dealership-mail-sender/src/main/java/eu/webdude/mailsender/Application.java
+++ car-dealership-mail-sender/src/main/java/eu/webdude/mailsender/Application.java
@@ -1,12 +1,14 @@
 package eu.webdude.mailsender;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
 @EnableJms
-public class Application {
+public class Application  {
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
---
+++ car-dealership-mail-sender/src/main/java/eu/webdude/mailsender/ConnectedMailSender.java
@@ -0,0 +1,48 @@
+package eu.webdude.mailsender;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+@Service
+public class ConnectedMailSender {
+	private JavaMailSender javaMailSender;
+
+	@Value("${email.destination}")
+	private String destination;
+
+	@Value("${email.sender}")
+	private String sender;
+
+	@Autowired
+	public ConnectedMailSender(JavaMailSender javaMailSender) {
+		this.javaMailSender = javaMailSender;
+	}
+
+	@JmsListener(destination = "message-queue")
+	public void messageListener(org.springframework.messaging.Message<String> message) {
+		System.out.printf("Message was sent with the following content: %n %s%n", message.getPayload());
+		sendEmail(message.getPayload());
+	}
+
+	private void sendEmail(String messageBody) {
+		MimeMessage mail = javaMailSender.createMimeMessage();
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(mail, true);
+			helper.setTo(destination);
+			helper.setReplyTo(sender);
+			helper.setFrom(sender);
+			helper.setSubject("A car has been deleted");
+			helper.setText(messageBody);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		javaMailSender.send(mail);
+	}
+}
